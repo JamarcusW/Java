6549ae6d4b803ad9caa3bf30e4d788e2f09d7e5a
JamarcusW <60477746+JamarcusW@users.noreply.github.com>
2020-02-20 10:42:09 -0500

    todo

--- Television.java
+++ Television.java
@@ -1,9 +1,51 @@
 
 public class Television {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private final String MANUFACTURER;
+	private final int SCREEN_SIZE; 
+	private boolean powerOn;
+	private int channel;
+	private int volume ;
+	
+		public Television (String brand, int size)
+		{
+			MANUFACTURER= brand;
+			SCREEN_SIZE = size;
+			powerOn = false ;
+			volume = 20 ;
+		}
+		
+		public void setChannel(int station)
+		{
+			
+			station = 0;
+			
+		}
+		
+		public void power()
+		{
+			
+			
+			
+		}
+		
+		public void increaseVolume()
+		{
+			
+			
+		}
+		
+		public void decreaseVolume()
+		{
+			
+			
+		}
 
-	}
+		public String getChannel() 
+		{
+			return station;
+			
+		}
 
 }
+
